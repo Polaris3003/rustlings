a0affd1c679a990ae845f9ba051fca85dd1de8cc
Polaris <2608726255@qq.com>
2024-12-29 21:10:01 +0800

    rustlings third time

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -23,11 +23,35 @@ enum Command {
     Append(usize),
 }
 
-mod my_module {
+pub mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = Vec::new();
+
+        for (s, cmd) in input {
+            match cmd {
+                Command::Uppercase => {
+                    output.push(s.to_uppercase());
+                }
+                Command::Trim => {
+                    output.push(s.trim().to_string());
+                }
+                Command::Append(n) => {
+                    let mut result = String::new();
+                    result.push_str(&s);
+                    for _ in 0..n {
+                        result.push_str("bar");
+                    }
+                    output.push(result);
+                }
+            }
+        }
+
+        output
+    }
 }
 
 fn main() {
@@ -39,7 +63,7 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
-
+    use crate::my_module::transformer;
     #[test]
     fn it_works() {
         let input = vec![
